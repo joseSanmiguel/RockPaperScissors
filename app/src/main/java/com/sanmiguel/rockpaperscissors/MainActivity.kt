a720a2729b5bf9f02b4bdf351004f04f2d82b142
Jose-sanmiguel <86948429+Jose-sanmiguel@users.noreply.github.com>
2022-04-11 21:19:47 +1200

    la aplicacion esta 100% funcional. falta: - Agregar el icono de la app - agregar todo el tema de ads de google para monetizar

---
+++ app/src/main/java/com/sanmiguel/rockpaperscissors/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.sanmiguel.rockpaperscissors
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //In this part of the code I setup Papel as default value for computer (ImageView).
+        val computer = findViewById<ImageView>(R.id.imaComputer)
+        computer.setImageResource(R.drawable.papel)
+
+        //In this part of the code I setup Piedra as default value for photoUser (ImageView).
+        val photoUser = findViewById<ImageView>(R.id.imaUser)
+        photoUser.setImageResource(R.drawable.piedra)
+
+        //code for Buttons
+        val tvWin = findViewById<TextView>(R.id.tvWinner)
+        val cValScore = findViewById<TextView>(R.id.cValScore)
+        val uValScore = findViewById<TextView>(R.id.uValScore)
+        var user = 0
+        var compu = 0
+        val stone = findViewById<ImageButton>(R.id.imaPiedra)
+        stone.setOnClickListener {
+            photoUser.setImageResource(R.drawable.piedra)
+            when((listOf("Stone", "Paper", "Scissor").random())){
+                "Stone" -> {computer.setImageResource(R.drawable.piedra)
+                    tvWin.text = "LIKE STONE"}
+                "Paper" -> {computer.setImageResource(R.drawable.papel)
+                    tvWin.text = "GOOD LUCK NEXT TIME"
+                    compu ++
+                    cValScore.text = compu.toString()
+                }
+                else -> {computer.setImageResource(R.drawable.tijeras)
+                    tvWin.text = "HEY YOU WON!!"
+                    user ++
+                    uValScore.text = user.toString()
+                }
+            }
+        }
+        val paper = findViewById<ImageButton>(R.id.imaPaper)
+        paper.setOnClickListener {
+            photoUser.setImageResource(R.drawable.papel)
+            when((listOf("Stone", "Paper", "Scissor").random())){
+                "Stone" -> {computer.setImageResource(R.drawable.piedra)
+                    tvWin.text = "HEY YOU WON!!"
+                    user ++
+                    uValScore.text = user.toString()
+                }
+                "Paper" -> {computer.setImageResource(R.drawable.papel)
+                    tvWin.text = "GIVE ME FIVE. WELL FOUR"}
+                else -> {computer.setImageResource(R.drawable.tijeras)
+                    tvWin.text = "GOOD LUCK NEXT TIME"
+                    compu ++
+                    cValScore.text = compu.toString()
+                }
+            }
+        }
+        val scissor = findViewById<ImageButton>(R.id.imaScissors)
+        scissor.setOnClickListener {
+            photoUser.setImageResource(R.drawable.tijeras)
+            when((listOf("Stone", "Paper", "Scissor").random())){
+                "Stone" -> {computer.setImageResource(R.drawable.piedra)
+                    tvWin.text = "GOOD LUCK NEXT TIME"
+                    compu ++
+                    cValScore.text = compu.toString()
+                }
+                "Paper" -> {computer.setImageResource(R.drawable.papel)
+                    tvWin.text = "HEY YOU WON!!"
+                    user ++
+                    uValScore.text = user.toString()
+                }
+                else -> {computer.setImageResource(R.drawable.tijeras)
+                    tvWin.text = "CROSS YOUR FINGERS"}
+            }
+        }
+        tvWin.setOnClickListener{
+            uValScore.text = "0"
+            cValScore.text = "0"
+            tvWin.text = "Touch me to Reset Scores"
+        }
+    }
+}
